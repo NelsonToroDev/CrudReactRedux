9e5e66e7af73dc5f3050ea0fa23681253896e98d
Nelson Toro <nelson.toro.flores@gmail.com>
2024-03-07 19:13:00 -0400

    Create useUserActions hook

--- src/components/ListOfUsers.tsx
+++ src/components/ListOfUsers.tsx
@@ -10,17 +10,13 @@ import {
   Title
 } from '@tremor/react';
 
-import { useAppDispatch, useAppSelector } from "../hooks/store";
-import { UserId, deleteUserById } from '../store/users/slice';
+import { useAppSelector } from "../hooks/store";
+import { useUserActions } from '../hooks/useUserActions';
 
 export function ListOfUsers () {
   const users = useAppSelector((state) => state.users);
 
-  const dispatch = useAppDispatch();
-
-  const handleRemoveUser = (id: UserId) => {
-    dispatch(deleteUserById(id));
-  }
+  const { removeUser } = useUserActions();
 
   return (
     <Card>
@@ -73,7 +69,7 @@ export function ListOfUsers () {
                     />
                   </svg>
                 </button>
-                <button onClick={() => handleRemoveUser(item.id)} type='button'>
+                <button onClick={() => removeUser(item.id)} type='button'>
                   <svg
                     aria-label='Remove element'
                     xmlns='http://www.w3.org/2000/svg'
