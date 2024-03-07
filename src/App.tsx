9f9e753125044b3acbb8db8115a64507e1cdee70
Nelson Toro <nelson.toro.flores@gmail.com>
2024-03-07 18:15:28 -0400

    Setting up Redux

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { ListOfUsers } from "./components/ListOfUsers";
+// "$schema": "https://docs.rome.tools/schemas/12.1.3/schema.json",
+function App () {
+
+	return (
+		<>
+			<ListOfUsers />
+		</>
+	);
+}
+
+export default App;
