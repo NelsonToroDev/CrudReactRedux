1f23e8e8308c45b4280f3ea0967c536e1678d3e0
Nelson Toro <nelson.toro.flores@gmail.com>
2024-03-08 16:47:15 -0400

    Implemented MiddleWares for logging and for Storage State

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { Middleware, configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users/slice";
 
+const loggerMiddleware: Middleware =
+	(store) => (next) => (action) => {
+		console.log(store.getState());
+		console.log(action);
+		next(action);
+		console.log(store.getState());
+	};
+
+const persistanceLocalStorageMiddleware: Middleware =
+	(store) => (next) => (action) => {
+		next(action);
+		localStorage.setItem("__redux__state__", JSON.stringify(store.getState()))
+	};
+
 export const store = configureStore({
-  reducer: {
-    users: usersReducer
-  }
+	reducer: {
+		users: usersReducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().prepend(loggerMiddleware).prepend(persistanceLocalStorageMiddleware),
 });
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -3,47 +3,55 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 export type UserId = string;
 
 export interface User {
-  name: string;
-  email: string;
-  github: string;
+	name: string;
+	email: string;
+	github: string;
 }
 
 export interface UserWithId extends User {
-  id: UserId
+	id: UserId;
 }
 
-const initialState: UserWithId[] = [
-  {
-    id: "1",
-    name: "Yazmand Rodriguez",
-    email: "yazmanito@gmail.com",
-    github: "yazmanito",
-  },
-  {
-    id: "2",
-    name: "John Doe",
-    email: "leo@gmail.com",
-    github: "leo",
-  },
-  {
-    id: "3",
-    name: "Haakon Dahlberg",
-    email: "haakon@gmail.com",
-    github: "midudev",
-  }
-]
+const DEFAULT_STATE: UserWithId[] = [
+	{
+		id: "1",
+		name: "Yazmand Rodriguez",
+		email: "yazmanito@gmail.com",
+		github: "yazmanito",
+	},
+	{
+		id: "2",
+		name: "John Doe",
+		email: "leo@gmail.com",
+		github: "leo",
+	},
+	{
+		id: "3",
+		name: "Haakon Dahlberg",
+		email: "haakon@gmail.com",
+		github: "midudev",
+	},
+];
+
+const initialState: UserWithId[] = (() => {
+	const persistedState = localStorage.getItem("__redux__state__");
+	if (persistedState) {
+		return JSON.parse(persistedState).users;
+	}
+
+	return DEFAULT_STATE;
+})();
 
 export const usersSlice = createSlice({
-  name: 'users',
-  initialState,
-  reducers: {
-    deleteUserById: (state, action: PayloadAction<UserId>) => {
-      const id = action.payload;
-      return state.filter((user) => user.id !== id);
-    }
-  }
-
-})
+	name: "users",
+	initialState,
+	reducers: {
+		deleteUserById: (state, action: PayloadAction<UserId>) => {
+			const id = action.payload;
+			return state.filter((user) => user.id !== id);
+		},
+	},
+});
 
 export default usersSlice.reducer;
 
